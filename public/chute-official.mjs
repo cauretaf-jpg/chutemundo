95f4d4ce3221e318b03eb717886d833d1e7ff5d8
cauretaf-jpg <cauretaf@gmail.com>
2026-07-14 12:33:28 -0400

    chore: cargar runtime Firestore 4.0.1

--- public/chute-official.mjs
+++ public/chute-official.mjs
@@ -1,3 +1,3 @@
-await import('/chute-official-loader.mjs?v=4.0.0');
+await import('/chute-official-loader.mjs?v=4.0.1');
 await import('/password-reset.mjs?v=3.0.3');
 await import('/chute-detail.mjs?v=4.0.0');
